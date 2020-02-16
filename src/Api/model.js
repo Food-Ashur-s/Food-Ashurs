0abadfe02179a98214ed14c07026cae1f5b02f01
Qusai-alhanaktah <qusaialhanaktah@gmail.com>
2020-02-16 14:21:34 +0200

    Added some JSDoc ,Nawal

--- src/Api/model.js
+++ src/Api/model.js
@@ -6,18 +6,37 @@ class Model {
   constructor(schema){
     this.schema = schema;
   }
+  /**
+   * get all users or a particular user
+   * @param {string} id 
+   */
   get(id){
     if(id) return this.schema.findById(id);
     else return this.schema.find({});
   }
 
+  /**
+   * post new record to our Database
+   * @param {object} record 
+   */
   create(record){
     let newRecord =  new this.schema(record);
     return newRecord.save();
   }
+
+  /**
+   * Update record by ID 
+   * @param {string} id 
+   * @param {Object} record 
+   */
   update(id, record){
     return this.schema.findByIdAndUpdate(id, record, {new: true});
   }
+
+  /**
+   * Delete record by ID 
+   * @param {String} id 
+   */
   delete(id){
     return this.schema.findByIdAndDelete(id);
   }
